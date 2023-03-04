6af9526d0b11c292f0bcfd114996a079d85e102a
medinadin <122950954+medinadin@users.noreply.github.com>
2023-03-03 21:16:09 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let menu = document.querySelector('#menu-bars');
+let header = document.querySelector('header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+    
+}
+
+document.querySelectorAll('a').forEach(links =>{
+
+    links.onmouseenter = () =>{
+        cursor1.classList.add('active');
+        cursor2.classList.add('active');
+    }
+
+});
